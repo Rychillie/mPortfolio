7d7393b8dcd37264116cb6a98bc6c1eba4838abd
Rychillie <rychillie@hotmail.com>
2021-11-30 20:10:55 -0300

    👌 IMPROVE: Add base Styles & Variables

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,17 @@ import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <header>
+        <h1>My App</h1>
+      </header>
+      <main>
+        <Component {...pageProps} />
+      </main>
+      <footer>Este é o Footer</footer>
+    </>
+  );
 }
 
 export default MyApp;
