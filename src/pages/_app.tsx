9a0d3956b6f4d281d324b77aa813d563d74f33ce
Rychillie <rychillie@hotmail.com>
2021-11-30 21:45:55 -0300

    👌 IMPROVE: SEO Default

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,33 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import Header from "../components/Header";
+import { DefaultSeo, NextSeo } from "next-seo";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <DefaultSeo
+        openGraph={{
+          type: "website",
+          locale: "en_US",
+          url: "https://mportfolio-nine.vercel.app/",
+          site_name: "Minimal Portfolio",
+          images: [
+            {
+              url: "/thumbnail.png",
+              alt: "Minimal Portfolio",
+              width: 842,
+              height: 595,
+              type: "image/png",
+            },
+          ],
+        }}
+        twitter={{
+          handle: "@rychillie",
+          site: "@rychillie",
+          cardType: "summary_large_image",
+        }}
+      />
       <Component {...pageProps} />
     </>
   );
